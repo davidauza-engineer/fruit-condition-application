21586680a7642326f623ae24bc30a692a5cfe63c
David Auza <davidauza01@gmail.com>
2022-12-07 01:47:57 -0500

    Update domains/fruit.py

--- domains/fruit.py
+++ domains/fruit.py
@@ -4,13 +4,16 @@ from statistics import mode
 from PIL import ImageColor
 from os import getcwd
 
-MODEL_PATH = getcwd() +'/assets/dt.pkl'
+MODEL_PATH = getcwd() +'/assets/fruit_classification.pkl'
 
+# This class represents a Fruit.
 class Fruit:
     def __init__(self, name, image):
         self.name = name
         self.image = image
 
+    # This method determines the condition of the fruit based on the data colors passed and our fruit classification
+    # model.
     def get_condition(self, data):
         loaded_model = pickle.load(open(MODEL_PATH, 'rb'))
         for i in range(0, len(data)):

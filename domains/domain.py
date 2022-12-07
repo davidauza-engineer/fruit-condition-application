bd4cb06b70ce9d2b7812d9029634459bc9677994
David Auza <davidauza01@gmail.com>
2022-12-07 01:44:55 -0500

    Update domains/domain.py

--- domains/domain.py
+++ domains/domain.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+# This class represents an interface of the methods that a domain should implement.
 class Domain(ABC):
     @abstractmethod
     def is_valid(self):

f2987539b54274a4a2fd0d6b1deec2869da06813
David Auza <davidauza01@gmail.com>
2022-12-07 01:44:10 -0500

    Update domains/color_palette.py

--- domains/color_palette.py
+++ domains/color_palette.py
@@ -3,6 +3,7 @@ import extcolors
 from colormap import rgb2hex
 
 
+# This function builds the dataframe with the `hex_code` and `total_pixels` based on the image colors.
 def build_colors_df(colors):
     colors_list = str(colors).replace('([(', '').split(', (')[0:-1]
     df_rgb = [i.split('), ')[0] + ')' for i in colors_list]
@@ -13,6 +14,7 @@ def build_colors_df(colors):
     return pd.DataFrame(zip(df_hex, df_percentage), columns=['hex_code', 'total_pixels'])
 
 
+# This class represents a color palette.
 class ColorPalette:
     def __init__(self, image):
         self.image = image
